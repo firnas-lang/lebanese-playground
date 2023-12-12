ba774a6d0469ec121c2899adbf0b2e25e5c40285
Hamza Jadid <haljadid@proximie.com>
2023-12-13 01:25:59 +0200

    chore: removed unused component

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from 'react';
-import { FiPlay, FiShare2 } from 'react-icons/fi'
+import { FiPlay } from 'react-icons/fi'
 
 interface ToolbarProps {
     onRun: Function,
     onShare: Function,
 }
 
-export const Toolbar = ({ onRun, onShare }: ToolbarProps) => {
+export const Toolbar = ({ onRun }: ToolbarProps) => {
     return (
         <div className="flex flex-row items-center">
             <div className="px-4">
