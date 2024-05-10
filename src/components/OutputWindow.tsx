c22a2e4ccd24049bc6b1da90b230f0a003db1705
Hamza Jadid <haljadid@proximie.com>
2024-05-10 22:43:15 +0300

    feat: added lebanese feature

--- src/components/OutputWindow.tsx
+++ src/components/OutputWindow.tsx
@@ -6,7 +6,7 @@ export const OutputWindow = ({ output }: OutputWindowProps) => {
     return (
         <div className="flex flex-col w-full">
             <h1 className="font-bold text-xl bg-clip-text text-transparent bg-gradient-to-r from-slate-900 to-slate-700 mb-2">
-                النواتج:
+                Nateji
             </h1>
             <pre className="w-full h-[30vh] bg-[#1e293b] rounded-md text-white font-normal text-sm overflow-y-auto px-2 py-1">
                 {output}
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,7 @@
 export const Title = () => {
     return (
         <h1 className="flex justify-center font-bold text-2xl bg-clip-text text-transparent bg-gradient-to-r from-slate-900 to-slate-700">
-            ملعب فرناس
+            Mal3ab 3abbis Ibn Firnes
         </h1>
     );
 }
